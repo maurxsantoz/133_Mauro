6271180d0ae1ecce01a4fce2acc9c725d46bec63
Mauro-Alexandre.COST <maurx18@gmail.com>
2018-11-26 10:17:05 +0100

    test git

---
+++ exo1/exo 1.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mauro-Alexandre.COST
+ * Date: 20.11.2018
+ * Time: 16:23
+ */
+$value=20;
+$step=3;
+$i;
+for($i=0;$i<10;$i++){
+    if($value>=30){
+        echo("Le nombre vaut .$value.<br>");
+    }else{
+        echo("nombre trop petit <br>");
+    }
+    $value+=$step;
+}
+?>
