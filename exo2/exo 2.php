a2175af5e20db9b2d6d1c4e521254d521722dfc6
Mauro-Alexandre.COST <maurx18@gmail.com>
2018-11-26 11:13:37 +0100

    Exercise 2

---
+++ exo2/exo 2.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mauro-Alexandre.COST
+ * Date: 26.11.2018
+ * Time: 10:32
+ */
+
+//Saturday 17 November 2018
+echo date("l j F Y") . "<br>";
+//Nov 17th 2018
+echo date("M jS Y") . "<br>";
+//17/11/18 11:16 pm
+echo date("j/m/y g:i a" ) . "<br>";
+//17 Nov 2018, 23:43:7
+echo date("j M Y, g:i:s" ) . "<br>";
+//Sat, 17 Nov 2018 23:48:47 +0100
+echo date(DATE_RFC822) . "<br>";
+?>
